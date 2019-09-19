c22f7fbfe780878990f3d5135cbcfd27e794be5b
amado123 <amado.penuelam@utadeo.edu.co>
2019-09-18 22:04:29 -0500

    v2 error default activity no found

--- app/src/main/java/com/example/appmovil/Micro.java
+++ app/src/main/java/com/example/appmovil/Micro.java
@@ -1,21 +1,18 @@
 package com.example.appmovil;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class MenuApp extends AppCompatActivity {
-    Button  btnsalir;
+public class Micro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_app);
+        setContentView(R.layout.activity_micro);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,20 +23,6 @@ public class MenuApp extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
-        configureNextButton();
-    }
-    private void configureNextButton(){
-        btnsalir = (Button) findViewById(R.id.Inicio);
-        btnsalir.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(),Micro.class);
-                startActivity(intent);
-              //  startActivity(new Intent(MenuApp.this,Micro.class));
-            }
-
         });
     }
 
---
+++ app/src/main/java/com/example/appmovil/MicroFragment.java
@@ -0,0 +1,22 @@
+package com.example.appmovil;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MicroFragment extends Fragment {
+
+    public MicroFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_micro, container, false);
+    }
+}
