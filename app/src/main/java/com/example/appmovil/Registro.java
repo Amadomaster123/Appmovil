f10421f61cb178c93e54744a1899dedbe1ea4921
amado123 <amado.penuelam@utadeo.edu.co>
2019-10-07 23:52:18 -0500

    FIRST CONNECT FIREBASE

--- app/src/main/java/com/example/appmovil/Registro.java
+++ app/src/main/java/com/example/appmovil/Registro.java
@@ -1,13 +1,84 @@
 package com.example.appmovil;
 
+import android.annotation.SuppressLint;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Registro extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class Registro extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    private EditText Textcorreo;
+    private EditText Textcontra;
+    private Button btnregis;
+
+    //objeto
+    private FirebaseAuth firebaseAuth;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+       /* firebaseAuth = FirebaseAuth.getInstance();
+        Textcorreo= (EditText) findViewById(R.id.correo);
+        Textcontra=(EditText) findViewById(R.id.password);
+        btnregis=(Button)findViewById(R.id.registrar);
+
+        btnregis.setOnClickListener(this);*/
+    }
+
+   /* private  void registrarusers(){
+        String corr=Textcorreo.getText().toString().trim();
+        String pass=Textcontra.getText().toString().trim();
+
+        //Verificamos que las cajas de texto no esten vacías
+        if(TextUtils.isEmpty(corr)){
+            Toast.makeText(this,"Se debe ingresar un email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        if(TextUtils.isEmpty(corr)){
+            Toast.makeText(this,"Falta ingresar la contraseña",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(corr, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+
+                            Toast.makeText(Registro.this,"Se ha registrado el usuario con el email: "+ Textcorreo.getText(),Toast.LENGTH_LONG).show();
+                        }else{
+
+                            Toast.makeText(Registro.this,"No se pudo registrar el usuario ",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }*/
+
+
+    @Override
+    public void onClick(View v) {
+
+
+       // registrarusers();
+
     }
 }
