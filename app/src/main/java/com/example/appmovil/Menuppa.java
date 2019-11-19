ff678a18dd642e3dbaddbe4a0135e981df542c90
amado123 <amado.penuelam@utadeo.edu.co>
2019-11-18 23:40:35 -0500

    menu ppa

--- app/src/main/java/com/example/appmovil/Menuppa.java
+++ app/src/main/java/com/example/appmovil/Menuppa.java
@@ -13,14 +13,6 @@ public class Menuppa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuppa);
-        btnpar = (Button) findViewById(R.id.parlante);
-        btnpar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Parlante.class);
-                startActivity(intent);
-            }
-        });
 
         btnsal = (Button) findViewById(R.id.salirmppa);
         btnsal.setOnClickListener(new View.OnClickListener() {
@@ -28,24 +20,6 @@ public class Menuppa extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Registro.class);
                 startActivity(intent);
-            }
-        });
-        btningfoto = (Button) findViewById(R.id.btnfoto);
-        btningfoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Foto.class);
-                startActivity(intent);
-            }
-        });
-
-        btntuto = (Button) findViewById(R.id.Tuto);
-        btntuto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Tutorial.class);
-                startActivity(intent);
-
             }
         });
     }
